902b48b603a76da5dda48628f2f698cd95679bbe
Ainvel <will_straw@ymail.com>
2018-04-10 17:44:04 +0700

    Merged

--- Main Folder/headers/Coin.hpp
+++ Main Folder/headers/Coin.hpp
@@ -12,21 +12,24 @@ private:
 public:
     Coin();
     Coin(int x, int y, int val, int xmax, int ymax); //memunculkan sebuah koin dengan posisi x y dengan value val.
+
     void moveDown(); //bergerak ke bawah secara terus menerus hingga sampai bawah.
     void moveDown(double sec_since_last); //bergerak ke bawah secara terus menerus hingga sampai bawah.
+
     bool isOnBottom() const; //mengecek apakah koin sudah sampai bawah (y = yMaxPos) atau belum.
     //getter setter
     int getValue() const;
     void setValue(int val);
     int getID();
+
     bool operator==(const Coin& C);
     bool operator==(std::nullptr_t n);
+
     int getNcoin();
 
     bool operator==(const Coin& C);
     bool operator!=(const Coin& C);
     bool operator==(std::nullptr_t n);
-
 
 };
 #endif
